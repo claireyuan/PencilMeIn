ec6894bf7a54520ca329ad2d0bcd22a47a154f65
Claire Yuan <claireyuan97@gmail.com>
2015-09-13 02:26:08 -0400

    added mscollectioncalendar files

--- PencilMeIn/PencilMeIn/ScheduleViewController.swift
+++ PencilMeIn/PencilMeIn/ScheduleViewController.swift
@@ -11,11 +11,6 @@ import EventKit
 
 class ScheduleViewController: UIViewController {
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
